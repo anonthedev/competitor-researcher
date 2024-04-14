2ee8b02cdfe86d49dcb62b5985e9ec4cad3e04a2
anon <opss59332@gmail.com>
2024-04-14 23:24:56 +0530

    cors fix

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "*"}})
 # app.config['CORS_HEADERS'] = 'Content-Type'
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
